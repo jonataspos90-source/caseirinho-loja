6d1e8120eb989407cac65adb380550469284c777
jonataspos90-source <jonataspos90@gmail.com>
2026-09-07 06:30:05 -0300

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,4 @@
+const CACHE='caseirinho-loja-v8-3-20260907';const SHELL=['./','./index.html'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',e=>{const u=new URL(e.request.url);if(/john-cloud-api-production\.up\.railway\.app/.test(u.host))return;if(e.request.method!=='GET')return;if(e.request.mode==='navigate'){e.respondWith(fetch(e.request,{cache:'no-store'}).then(r=>{caches.open(CACHE).then(c=>c.put('./index.html',r.clone())).catch(()=>{});return r}).catch(()=>caches.match('./index.html')));return}e.respondWith(caches.match(e.request).then(x=>x||fetch(e.request)))})
