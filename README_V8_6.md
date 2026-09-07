e891940894951c2a2bf72d084662b9e85e1c4bb3
jonataspos90-source <jonataspos90@gmail.com>
2026-09-07 19:03:20 -0300

    Add files via upload

---
+++ README_V8_6.md
@@ -0,0 +1,7 @@
+# Caseirinho Loja V8.6.0
+
+- Checkout por CEP: rua, bairro, cidade e UF preenchidos via API; cliente informa o número.
+- Frete automático pelas regras publicadas do Cadastro de Frete do ERP; sem regra = cotação pendente no ERP.
+- Produtos exibem a descrição publicada pelo ERP.
+- Toda foto de produto mostra “Imagem meramente ilustrativa”.
+- Consentimento LGPD obrigatório conforme política publicada pelo ERP e acesso à Política de Privacidade.
--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE="caseirinho-loja-v8-4-4-20260907-pix";
+const CACHE="caseirinho-loja-v8-6-0-20260907-cep-lgpd";
 const SHELL=['./','./index.html','./manifest.webmanifest','./icons/icon-192.png','./icons/icon-512.png','./icons/icon-maskable-512.png','./icons/apple-touch-icon.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
