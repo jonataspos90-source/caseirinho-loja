8706ac0dd1af84dec6233885e5c4c016e765161a
jonataspos90-source <jonataspos90@gmail.com>
2026-09-15 14:30:40 -0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -4,6 +4,7 @@ const SHELL=[
   './index.html',
   './styles.css',
   './app.js',
+  './commerce-engine-v9-3-0.js',
   './manifest.webmanifest',
   './icons/icon-192.png',
   './icons/icon-512.png',
